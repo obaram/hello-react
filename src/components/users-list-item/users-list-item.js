e5bd398f013de0cce6f13cd91cdd0fcd06b060d9
Mateusz Obara <mateusz@processbliss.com>
2022-12-28 21:11:24 +0100

    eventy

--- src/components/users-list-item/users-list-item.js
+++ src/components/users-list-item/users-list-item.js
@@ -4,16 +4,20 @@ import BadgeCircle from "../badge-circle/badge-circle-";
 import Wrapper from "./wrapper.styles";
 
 
-const  UsersListItem = (props) => (
+const  UsersListItem = (props) => {
+    const buttonClicked = (index) => alert(`User ${index} clicked`)
+
+    return (
     <Wrapper>
         <BadgeCircle value={props.user.average}/>
         <div>
             <p>{props.user.name}</p>
             <p>attendance: {props.user.attendance}</p>
         </div>
-        <Button/>
+        <Button onClick={() => buttonClicked(props.index)}/>
     </Wrapper>
- )
+    )
+}
 
 export default UsersListItem;
 
